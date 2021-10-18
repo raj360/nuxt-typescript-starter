bd2112590631376a27ae1fdc156bdd1e8bfb201c
Raymond <rayjxclusive@gmail.com>
2021-10-18 20:13:56 +0300

    update defined User state Module

---
+++ store/modules/user.ts
@@ -0,0 +1,15 @@
+import {Module, VuexModule} from "vuex-module-decorators"
+
+@Module({namespaced:true,stateFactory:true,name:"Greetings"})
+export default class userModule extends VuexModule {
+  user = {
+    fullName: "Raymond Kalumba",
+    role: { HumanLivingBoilerPlate: true },
+    active: true,
+    uid: 12345
+  };
+
+  getUser(){
+   return {...this.user}
+  }
+}
